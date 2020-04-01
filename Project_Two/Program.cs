260207ad590706f84881f97527737d7e9eb41896
Jake Cermak <cerjakt@dunwoody.edu>
2020-04-01 18:28:50 -0500

    added a print winning teams function

--- Project_Two/Program.cs
+++ Project_Two/Program.cs
@@ -10,7 +10,7 @@ namespace Project_Two
         {
             //Reading from a file
             //Declaring variables
-            const string PATH = @"C:\Users\Jake\Documents\College\SENG Semester 2\Advanced Programming\Projects\Project_Two\Super_Bowl_Project.csv";
+            const string PATH = @"C:\Users\Jake\Documents\College\SENG Semester 2\Advanced Programming\Projects\Project 2\Project_Two\Super_Bowl_Project.csv";
 
             FileStream input;
             StreamReader read;
@@ -22,27 +22,34 @@ namespace Project_Two
                 input = new FileStream(PATH, FileMode.Open, FileAccess.Read);
                 read = new StreamReader(input);
                 line = read.ReadLine();
-                List<superBowl> sbDataList = new List<superBowl>();
+                List<SuperBowl> sbDataList = new List<SuperBowl>();
 
                 //Establishing a looping structure to read in all the superbowl data
                 while (!read.EndOfStream)
                 {
                     sbData = read.ReadLine().Split(',');
-                    sbDataList.Add(new superBowl(sbData[0], sbData[1], Convert.ToInt32(sbData[2]), sbData[3], sbData[4], sbData[5], Convert.ToInt32(sbData[6]),
-                                   sbData[7], sbData[8], sbData[9], Convert.ToInt32(sbData[10]), sbData[11], sbData[12], sbData[13], sbData[14]));
-                    Console.WriteLine(sbDataList[sbDataList.Count -1]);
+                    SuperBowl sb = new SuperBowl(sbData);
+                    sbDataList.Add(sb);
+                    //Console.WriteLine(sbDataList[sbDataList.Count -1]); //used to insure that the data is populating the list
                 }
 
                 read.Dispose();
                 input.Dispose();
+
+                foreach (SuperBowl sb in sbDataList)
+                {
+                    Console.WriteLine(sb.printWinner());
+                }
             }
 
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
+
+
         }
-        class superBowl //Making a class for the superbowl data
+        class SuperBowl //Making a class for the superbowl data
         {
             public string Date { get; set; }
             public string SB { get; set; }
@@ -61,24 +68,30 @@ namespace Project_Two
             public string State { get; set; }
 
             //Making a constructor
-            public superBowl(string Date, string SB, int Attendance, string winningQB, string winningCoach, string winningTeam, int winningPoints,
-                             string losingQB, string losingCoach, string losingTeam, int losingPoints, string MVP, string Stadium, string City, string State)
+            public SuperBowl(string[] sbData)
             {
-                this.Date = Date;
-                this.SB = SB;
-                this.Attendance = Attendance;
-                this.winningQB = winningQB;
-                this.winningCoach = winningCoach;
-                this.winningTeam = winningTeam;
-                this.winningPoints = winningPoints;
-                this.losingQB = losingQB;
-                this.losingCoach = losingCoach;
-                this.losingTeam = losingTeam;
-                this.losingPoints = losingPoints;
-                this.MVP = MVP;
-                this.Stadium = Stadium;
-                this.City = City;
-                this.State = State;
+                this.Date = sbData[0];
+                this.SB = sbData[1];
+                this.Attendance = Convert.ToInt32(sbData[2]);
+                this.winningQB = sbData[3];
+                this.winningCoach = sbData[4];
+                this.winningTeam = sbData[5];
+                this.winningPoints = Convert.ToInt32(sbData[6]);
+                this.losingQB = sbData[7];
+                this.losingCoach = sbData[8];
+                this.losingTeam = sbData[9];
+                this.losingPoints = Convert.ToInt32(sbData[10]);
+                this.MVP = sbData[11];
+                this.Stadium = sbData[12];
+                this.City = sbData[13];
+                this.State = sbData[14];
+            }
+            public string printWinner()
+            {
+                return String.Format($"{winningTeam} won the Superbowl in {Date}. " +
+                    $"Their quarterback was {winningQB}. Their winning coach was {winningCoach}." +
+                    $"The MVP was {MVP}. The point difference was {winningPoints - losingPoints}.\n");
+                
             }
 
             //Defining toString
